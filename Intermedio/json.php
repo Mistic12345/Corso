e2a4ccf2be55fa7c53d8c34adf81616f8aa80a19
PaoloBasile <paolobasile37@gmail.com>
2026-06-26 18:02:08 +0200

    Update courses

---
+++ Intermedio/json.php
@@ -0,0 +1,372 @@
+<?php
+// Json serve per salvare e scambiare dati
+$utente = [
+  [
+    "atletaId"=> 1,
+    "nome"=> "Marco",
+    "cognome"=> "Rossi",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Milano",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 2,
+    "nome"=> "Luca",
+    "cognome"=> "Bianchi",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Roma",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 3,
+    "nome"=> "Giulia",
+    "cognome"=> "Verdi",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Torino",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 4,
+    "nome"=> "Sara",
+    "cognome"=> "Neri",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Napoli",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 5,
+    "nome"=> "Paolo",
+    "cognome"=> "Ferrari",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Bologna",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 6,
+    "nome"=> "Andrea",
+    "cognome"=> "Costa",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Firenze",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 7,
+    "nome"=> "Chiara",
+    "cognome"=> "Galli",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Genova",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 8,
+    "nome"=> "Elisa",
+    "cognome"=> "Fontana",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Padova",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 9,
+    "nome"=> "Davide",
+    "cognome"=> "Greco",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Bari",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 10,
+    "nome"=> "Marta",
+    "cognome"=> "Sala",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Cagliari",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 11,
+    "nome"=> "Simone",
+    "cognome"=> "Riva",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Parma",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 12,
+    "nome"=> "Alessia",
+    "cognome"=> "Serra",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Modena",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 13,
+    "nome"=> "Giorgio",
+    "cognome"=> "Testa",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Trieste",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 14,
+    "nome"=> "Francesca",
+    "cognome"=> "Villa",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Pisa",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 15,
+    "nome"=> "Matteo",
+    "cognome"=> "Moretti",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Perugia",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 16,
+    "nome"=> "Ilaria",
+    "cognome"=> "Colombo",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Rimini",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 17,
+    "nome"=> "Stefano",
+    "cognome"=> "Martini",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Lecce",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 18,
+    "nome"=> "Valentina",
+    "cognome"=> "Longo",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Udine",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 19,
+    "nome"=> "Fabio",
+    "cognome"=> "De Luca",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Trento",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 20,
+    "nome"=> "Elena",
+    "cognome"=> "Marini",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Aosta",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 21,
+    "nome"=> "Nicola",
+    "cognome"=> "Pellegrini",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Mantova",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 22,
+    "nome"=> "Silvia",
+    "cognome"=> "Pagani",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Ferrara",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 23,
+    "nome"=> "Daniele",
+    "cognome"=> "Rizzo",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Reggio Emilia",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 24,
+    "nome"=> "Martina",
+    "cognome"=> "Fabbri",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Forlì",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 25,
+    "nome"=> "Gabriele",
+    "cognome"=> "Palmieri",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Savona",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 26,
+    "nome"=> "Alice",
+    "cognome"=> "Monti",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "La Spezia",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 27,
+    "nome"=> "Riccardo",
+    "cognome"=> "Fiore",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Verona",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 28,
+    "nome"=> "Beatrice",
+    "cognome"=> "Sanna",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Sassari",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 29,
+    "nome"=> "Tommaso",
+    "cognome"=> "Guerra",
+    "sesso"=> "M",
+    "dataNascita"=> null,
+    "citta"=> "Lodi",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ],
+  [
+    "atletaId"=> 30,
+    "nome"=> "Laura",
+    "cognome"=> "Caruso",
+    "sesso"=> "F",
+    "dataNascita"=> null,
+    "citta"=> "Pavia",
+    "nazionalita"=> "Italiana",
+    "certificatiAtletis"=> [],
+    "iscrizionis"=> []
+  ]
+];
+
+// converzione l'array in json
+$jsonAPI = json_encode($utente);
+
+$cartella = "data";
+$path = $cartella . "/utenti.json";
+
+// Se non esiste lo crea 
+// mkdir(data) crea una nuova cartella 
+// Il permesso 0777 indica i permessi
+// Il parametro true  permette anche di creare le cartelle annidate 
+if(!is_dir($cartella))
+{
+    mkdir($cartella, 0777, true);
+}
+
+
+// Se non esiste lo crea e scrive un array Json vuoto
+if(!file_exists($path))
+{
+    file_put_contents($path, "[]");
+}
+
+// Salvataggio nel file utenti.json
+file_put_contents($path, $jsonAPI);
+
+echo "<h5>Il salvataggio è andata a buon fine</h5>";
+
+
+// echo "<h3>JSON</h3>";
+// echo ($jsonAPI);
+
+// // C    ONVERTE DA JSON IN ARRAY
+// $arrayPhp = json_decode($jsonAPI, true);
+
+// echo "<h3>ARRAY PHP</h3>";
+
+// print_r($arrayPhp);
